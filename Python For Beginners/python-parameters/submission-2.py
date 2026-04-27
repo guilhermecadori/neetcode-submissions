dba18d29fbf2b08ea8c25a0c10d7f05124377ac4
Guilherme Cadori <61103271+guilhermecadori@users.noreply.github.com>
2026-04-26 21:26:18 -0300

    Add: python-parameters - submission-2

---
+++ Python For Beginners/python-parameters/submission-2.py
@@ -0,0 +1,12 @@
+
+# def farewell(name):
+#     print("Goodbye, " + name)
+
+def farewell(name):
+    msg = f"Goodbye, {name}"
+    return print(msg)
+
+# don't modify below this line
+farewell("Bob")
+farewell("Charlie")
+farewell("NeetCode")
