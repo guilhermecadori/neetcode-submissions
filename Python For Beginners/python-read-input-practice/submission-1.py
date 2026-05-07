c99607e20e10d6db56b11b243884d197c86db232
Guilherme Cadori <61103271+guilhermecadori@users.noreply.github.com>
2026-05-07 18:21:50 -0300

    Add: python-read-input-practice - submission-1

---
+++ Python For Beginners/python-read-input-practice/submission-1.py
@@ -0,0 +1,13 @@
+def add_two_numbers() -> int:
+    args_chars = input()
+    args_nums = args_chars.split(",")
+    arg_list = []
+    for arg in args_nums:
+        arg_list.append(int(arg))
+    return sum(arg_list)
+
+# do not modify below this line
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
