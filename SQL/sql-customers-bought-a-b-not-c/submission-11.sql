c4ac910c171dea0e3dcef3b048bfc522239b0b0e
Guilherme Cadori <61103271+guilhermecadori@users.noreply.github.com>
2026-05-19 21:05:49 -0300

    Add: sql-customers-bought-a-b-not-c - submission-11

---
+++ SQL/sql-customers-bought-a-b-not-c/submission-11.sql
@@ -0,0 +1,30 @@
+-- Write your query below
+-- SELECT c.customer_id, c.customer_name
+-- FROM customers c
+-- WHERE
+--     c.customer_id IN (SELECT customer_id FROM orders WHERE product_name = 'A')
+-- AND c.customer_id IN (SELECT customer_id FROM orders WHERE product_name = 'B')
+-- AND c.customer_id NOT IN (SELECT customer_id FROM orders WHERE product_name = 'C')
+-- ORDER BY c.customer_name;
+
+SELECT c.customer_id, c.customer_name
+FROM customers c
+WHERE EXISTS (
+    SELECT 1
+    FROM orders o
+    WHERE o.customer_id = c.customer_id
+      AND o.product_name = 'A'
+)
+AND EXISTS (
+    SELECT 1
+    FROM orders o
+    WHERE o.customer_id = c.customer_id
+      AND o.product_name = 'B'
+)
+AND NOT EXISTS (
+    SELECT 1
+    FROM orders o
+    WHERE o.customer_id = c.customer_id
+      AND o.product_name = 'C'
+)
+ORDER BY c.customer_name;
