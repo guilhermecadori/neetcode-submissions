27520ace9bc12ec1ce101cab1a99a195252556d8
Guilherme Cadori <61103271+guilhermecadori@users.noreply.github.com>
2026-05-22 00:56:09 -0300

    Add: sql-sellers-with-no-sales - submission-3

---
+++ SQL/sql-sellers-with-no-sales/submission-3.sql
@@ -0,0 +1,17 @@
+-- -- Write your query below
+-- SELECT s.seller_name
+-- FROM seller s
+-- LEFT JOIN orders o
+-- ON s.seller_id = o.seller_id
+-- AND o.sale_date BETWEEN '2020-01-01' AND '2020-12-31'
+-- WHERE o.order_id IS NULL
+-- ORDER BY s.seller_name;
+
+SELECT s.seller_name
+FROM seller s
+WHERE s.seller_id NOT IN (
+    SELECT DISTINCT seller_id
+    FROM orders
+    WHERE sale_date >= '2020-01-01' AND sale_date <= '2020-12-31'
+)
+ORDER BY s.seller_name ASC;
